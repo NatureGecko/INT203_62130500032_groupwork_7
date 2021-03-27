7eb8bfbb5442992891df8c8d95590b479f2a879f
naturegecko <thanaphat.ns@mail.kmutt.ac.th>
2021-03-27 22:13:08 +0700

    Update main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        amber : {
+          c600 : '#D97706',
+          c800 : '#92400E'
+        }
+      }
+      
+    },
   },
   variants: {
     extend: {},
