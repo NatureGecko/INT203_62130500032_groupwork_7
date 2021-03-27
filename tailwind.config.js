1571d90409a27285b330f804fb571835dc3039fb
naturegecko <thanaphat.ns@mail.kmutt.ac.th>
2021-03-27 22:34:52 +0700

    Finish the main brance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors:{
-        amber : {
-          c600 : '#D97706',
-          c800 : '#92400E'
+        amber:{
+          c600:'#D97706',
+          c800:'#92400E'
+        },
+        laxury:{
+          blue :'#3B71CE',
+          yellowlight: '#EFE692',
+          bluedark:'#13223E'
         }
       }
-      
     },
   },
   variants: {
